660b768d7c79de588495b85e12f6f89a96c50ea2
Rohith Vishwa <rohith.s@redim.net>
2025-07-09 11:28:12 +0530

    Intial commit. Initialized Springboot project

---
+++ Qino/src/main/java/com/example/Qino/controller/GameController.java
@@ -0,0 +1,11 @@
+package com.example.Qino.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Time;
+
+@RestController
+public class GameController {
+}
